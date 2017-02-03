0263925f82595a116724d2ad6c57d24cb4b30917
Evan Schlachter <Evan.Schlachter@gmail.com>
2017-02-03 13:16:04 -0800

    print date

--- src/Evan/Schlachter/GitHub/SeemedHarmless.java
+++ src/Evan/Schlachter/GitHub/SeemedHarmless.java
@@ -1,11 +1,14 @@
 package Evan.Schlachter.GitHub;
 
+import java.util.Date;
+
 /**
- * Created by soume_000 on 2/3/2017.
+ * Created by Evan_Schlachter on 2/3/2017.
  */
 public class SeemedHarmless {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         System.out.println("The square root of 912.04 is 30.2");
         System.out.println("It all seemed so harmless...");
+        System.out.println(new Date());
     }
 }
