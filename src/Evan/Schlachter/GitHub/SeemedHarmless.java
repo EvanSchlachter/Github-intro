8e8daa01d4bd691a17665960d2afc3a2f0b902fc
Evan Schlachter <Evan.Schlachter@gmail.com>
2017-02-03 13:03:56 -0800

    added a method that prints out a seemingly harmless message

---
+++ src/Evan/Schlachter/GitHub/SeemedHarmless.java
@@ -0,0 +1,11 @@
+package Evan.Schlachter.GitHub;
+
+/**
+ * Created by soume_000 on 2/3/2017.
+ */
+public class SeemedHarmless {
+    public static void main (String[] args){
+        System.out.println("The square root of 912.04 is 30.2");
+        System.out.println("It all seemed so harmless...");
+    }
+}
--- src/Evan/Schlachter/GitHub/Start/GitHubStart.java
+++
@@ -1,7 +0,0 @@
-package Evan.Schlachter.GitHub.Start;
-
-/**
- * Created by soume_000 on 2/3/2017.
- */
-public class GitHubStart {
-}
